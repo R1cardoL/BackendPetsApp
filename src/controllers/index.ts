3281814b6315f5fbf78f358b3fdd3f7be60f86b7
R1cardoL <andresrlopez401@gmail.com>
2022-10-29 08:12:52 -0500

    Se crean los repositorios y las relaciones

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,5 @@
 export * from './ping.controller';
+export * from './mascota-usuario.controller';
+export * from './usuario-mascota.controller';
+export * from './plan-mascota.controller';
+export * from './mascota-plan.controller';
